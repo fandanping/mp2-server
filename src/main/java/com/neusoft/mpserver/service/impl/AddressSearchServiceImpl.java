50d7e827afb28408979b3fb3fa444cf7c284c9cb
fandanping <18602474229@163.com>
2018-07-23 19:17:47 +0800

    change11

--- src/main/java/com/neusoft/mpserver/service/impl/AddressSearchServiceImpl.java
+++ src/main/java/com/neusoft/mpserver/service/impl/AddressSearchServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.text.SimpleDateFormat;
 import java.util.*;
-
 /**
  * 地址标引-查询模块：service层实现
  * 只有在这一层加事务管理才是真正的事务管理
