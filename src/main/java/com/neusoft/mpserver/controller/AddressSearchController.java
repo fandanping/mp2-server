95f2d2cc76f2f56586d5e9cb135201278af5f88e
fandanping <18602474229@163.com>
2018-07-23 19:19:14 +0800

    change11

--- src/main/java/com/neusoft/mpserver/controller/AddressSearchController.java
+++ src/main/java/com/neusoft/mpserver/controller/AddressSearchController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 /**
  * address:查询未标引地址列表模块
  *
@@ -26,47 +27,47 @@ public class AddressSearchController {
 
     /**
      * 登录后初次进首页，查询正在标引的地址列表，如果没有正在标引的，则随机20篇：controller层
+     *
      * @param request
      * @return
      */
     @GetMapping("/search/marking")
-    public Map<String,Object> searchMarking(HttpServletRequest request){
+    public Map<String, Object> searchMarking(HttpServletRequest request) {
         String userId = (String) request.getAttribute(Constant.USER_ID);
         return addressSearchService.showMarkingList(userId);
     }
 
     /**
      * 点击随标按钮，随机20篇出来，也可通过关键词模糊匹配
+     *
      * @param keyword
      * @param request
      * @return
      */
     @GetMapping("/search/random")
-     public Map<String,Object>  searchUnMark(String keyword,HttpServletRequest request){
+    public Map<String, Object> searchUnMark(String keyword, HttpServletRequest request) {
         String userId = (String) request.getAttribute(Constant.USER_ID);
-        return addressSearchService.showUnMarkList(userId,keyword);
+        return addressSearchService.showUnMarkList(userId, keyword);
     }
 
     /**
      * 保存词
+     *
      * @param postMap
      * @param request
      * @return
      */
     @PostMapping("/save")
-    public Map<String ,Object> addAddress(@RequestBody Map postMap, HttpServletRequest request){
+    public Map<String, Object> addAddress(@RequestBody Map postMap, HttpServletRequest request) {
         Gson gson = new Gson();
         String markStr = (String) postMap.get("markAddressList");
-        List<AddressMarkForm> markList = gson.fromJson(markStr, new TypeToken<List<AddressMarkForm>>(){}.getType());
+        List<AddressMarkForm> markList = gson.fromJson(markStr, new TypeToken<List<AddressMarkForm>>() {
+        }.getType());
         String userId = (String) request.getAttribute(Constant.USER_ID);
-        boolean flag=addressSearchService.addMark(userId,markList);
-        Map<String ,Object> map=new HashMap<String,Object>();
-        map.put("flag",flag);
+        boolean flag = addressSearchService.addMark(userId, markList);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("flag", flag);
         return map;
     }
-
-
-
-
 
 }
