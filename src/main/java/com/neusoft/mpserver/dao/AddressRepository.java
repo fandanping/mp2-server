b8610ba1816dd09df24329d4853e429de821758a
fandanping <18602474229@163.com>
2018-07-25 07:58:57 +0800

    change11

--- src/main/java/com/neusoft/mpserver/dao/AddressRepository.java
+++ src/main/java/com/neusoft/mpserver/dao/AddressRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
-
 /**
  * 地址模块数据库操作层--地址表
  */
@@ -20,6 +19,8 @@ public interface AddressRepository extends JpaRepository<AddressMark,String>{
 
     //随机top20
    // @Query(value="select id, an, address, zip from (select nvl(qid,' ') as id, nvl(an, ' ') as an,  nvl(app_address, ' ') as address, nvl(app_zip, ' ') as zip from sipo_ap_address_test where marked is null order by dbms_random.value) where rownum <=20", nativeQuery=true)
+   // @Query(value="select nvl(qid,' ') ,nvl(an, ' '),nvl(app_address, ' ') ,nvl(app_zip, ' ') from sipo_ap_address_test where marked is null  and rownum <=20", nativeQuery=true)
+    //@Query(value="select id ,an,address ,zip from AddressMark where marked is null  and rownum <=20")
     @Query(value="select nvl(qid,' ') ,nvl(an, ' '),nvl(app_address, ' ') ,nvl(app_zip, ' ') from sipo_ap_address_test where marked is null  and rownum <=20", nativeQuery=true)
     public List<Object[]> findByRandom();
 
