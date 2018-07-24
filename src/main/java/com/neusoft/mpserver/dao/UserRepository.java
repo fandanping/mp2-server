d2d7aca248f9e0c0129bbaefc3487d137af10303
fandanping <18602474229@163.com>
2018-07-24 08:01:47 +0800

    change11

--- src/main/java/com/neusoft/mpserver/dao/UserRepository.java
+++ src/main/java/com/neusoft/mpserver/dao/UserRepository.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 /**
  * user操作数据库层
+ *
  * @name fandp
  * @email fandp@neusoft.com
  */
-public interface UserRepository   extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User, String> {
     public List<User> findByUsername(String username);
+
     public User findByUsernameAndPassword(String username, String password);
 }
